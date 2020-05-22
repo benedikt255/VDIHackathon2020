558bf1b3790c143195ad6c623e56ce010cb6864a
Mioedvitnir <gr3lly@gmail.com>
2020-05-22 22:40:19 +0200

    Update IChannelMgmt.ts

--- VDIApp/src/interface/IChannelMgmt.ts
+++ VDIApp/src/interface/IChannelMgmt.ts
@@ -1,3 +1,5 @@
+import { IConnectIngPost } from "./IPostMgmt";
+
 interface IConnectIngChannel{
 
     id : String
@@ -5,6 +7,7 @@ interface IConnectIngChannel{
     description : String
     picture : String
     persons  : String[]
+    posts : IConnectIngPost[]
 
     createChannel() : boolean
     updateChannel() : boolean
@@ -13,7 +16,37 @@ interface IConnectIngChannel{
 }
 
 class mockChannel implements IConnectIngChannel{
+    id: String;
+    name: String;
+    description: String;
+    picture: String;
+    persons: String[];
+    posts: any[];
 
+    createChannel(): boolean {
+        return false;
+    }
 
+    private constructor(){
+        this.id = "007"
+        this.name = "GoldenEye"
+        this.description = "you know this!"
+        this.picture = ""
+        this.persons = [""]
+        this.posts = [""]
+    }
+
+    updateChannel(): boolean {
+        this.description = "updated"
+        return true;
+    }
+
+    getChannel(): IConnectIngChannel {
+        return this
+    }
+
+    removePost(postId: String): boolean {
+        throw new Error("Method not implemented.");
+    }
 
 }
