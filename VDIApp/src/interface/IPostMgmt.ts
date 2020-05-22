9cd6ecb30b274362492af39b81b5cd0c3004d4c0
AgentPinky <koeble.soeren@vdi.de>
2020-05-22 22:14:12 +0200

    zweiter punkt

--- VDIApp/src/interface/IPostMgmt.ts
+++ VDIApp/src/interface/IPostMgmt.ts
@@ -1,11 +1,20 @@
+import { IConnectIngComment } from "./ICommentMgmt";;
 
-
+// Interface - IConnectIngPost
+// It represents a post under a person
 interface IConnectIngPost {
+    // Post Identifier
     id: string;
+    // Author Identifier
     authorId: string;
+    // Author Identifier
     authorIdCreation: string;
-    Timestamp: Date;
-    title: () => string;
-    message: () => string;
-    comments: IConnectIngComment;
+    // post Creation Timestamp
+    creationTS: Date;
+    // Post Title
+    title: string;
+    // Post Text
+    message: string;
+    // Comments to Psot
+    comments: Array<IConnectIngComment>;
 }
