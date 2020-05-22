0d010fee4c615850028d18852cf6fb5651823502
benedikt255 <benedikt.geib@googlemail.com>
2020-05-22 22:09:13 +0200

    + comments

--- VDIApp/src/interface/IUserMgmt.ts
+++ VDIApp/src/interface/IUserMgmt.ts
@@ -1,16 +1,18 @@
+// data type of user with all attributes
 export class ConnectingUser {
-  id!: string;
-  firstName!: string;
-  lastName!: string;
-  location!: string;
+  id!: string; // (internal) id of the user
+  firstName!: string; // first name
+  lastName!: string; // last name
+  location!: string; // location of the user
   image!: string; // base64 encoded image
-  jobTitle!: string;
-  email!: string;
+  jobTitle!: string; // job title of the user
+  email!: string; // email address of the user
 }
 
+// interface for managing users
 export interface IUserManagement {
-  connectUser(email: string, password: string): string; // login
-  disconnectUser(): boolean; // logout
+  connectUser(email: string, password: string): string; // login user
+  disconnectUser(): boolean; // logout user
   registerUser(user: ConnectingUser, password: string): string; // create user
   unregisterUser(email: string): boolean; // delete user
   updateUser(user: ConnectingUser): boolean; // change user data
