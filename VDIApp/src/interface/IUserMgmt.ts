30982044e19a23617043391094edfba0eb608d2c
benedikt255 <benedikt.geib@googlemail.com>
2020-05-23 00:41:12 +0200

    + token

--- VDIApp/src/interface/IUserMgmt.ts
+++ VDIApp/src/interface/IUserMgmt.ts
@@ -1,57 +1,61 @@
 // data type of user with all attributes
-export class ConnectingUser {
+export class ConnectIngUser {
   readonly id!: string; // (internal) id of the user
+  username!: string; // username
   firstName!: string; // first name
   lastName!: string; // last name
   location!: string; // location of the user
   image!: string; // base64 encoded image
   jobTitle!: string; // job title of the user
   email!: string; // email address of the user
+  token!: string; // token for API calls
 }
 
 // interface for managing users
 export interface IUserManagement {
-  connectUser(email: string, password: string): string; // login user
-  disconnectUser(): boolean; // logout user
-  registerUser(user: ConnectingUser, password: string): string; // create user
-  unregisterUser(email: string): boolean; // delete user
-  updateUser(user: ConnectingUser): boolean; // change user data
-  getUsers(): ConnectingUser[]; // not implemented
-  getUser(): ConnectingUser; // gets user data of current user
+  connectUser(username: string, password: string): ConnectIngUser; // login user
+  disconnectUser(user: ConnectIngUser): boolean; // logout user
+  registerUser(user: ConnectIngUser, password: string): string; // create user
+  unregisterUser(user: ConnectIngUser): boolean; // delete user
+  updateUser(user: ConnectIngUser): boolean; // change user data
+  getUsers(user: ConnectIngUser): ConnectIngUser[]; // not implemented
+  getUser(user: ConnectIngUser): ConnectIngUser; // gets user data of current user
 }
 
 // mock class for UI tests
 export class MockUserManagement implements IUserManagement {
-  private user: ConnectingUser = {
+  private user: ConnectIngUser = {
       id: '1',
       firstName: 'Mustermann',
       lastName: 'Max',
       location: 'Berlin',
       image: '',
       jobTitle: 'Ingenieur',
-      email: 'Max.Mustermann@example.com'
+      email: 'Max.Mustermann@example.com',
+      username: 'user1',
+      token: 'Basic 12345'
   };
 
-  connectUser(email: string, password: string): string {
-    return 'Basic 12345';
+  connectUser(username: string, password: string): ConnectIngUser {
+    return this.user;
   }
-  disconnectUser(): boolean {
+  disconnectUser(user: ConnectIngUser): boolean {
     return true;
   }
-  registerUser(user: ConnectingUser, password: string): string {
+  registerUser(user: ConnectIngUser, password: string): string {
     return '1';
   }
-  unregisterUser(email: string): boolean {
+  unregisterUser(user: ConnectIngUser): boolean {
     return true;
   }
-  updateUser(user: ConnectingUser): boolean {
+  updateUser(user: ConnectIngUser): boolean {
     this.user = user;
     return true;
   }
-  getUsers(): ConnectingUser[] {
+  getUsers(user: ConnectIngUser): ConnectIngUser[] {
     throw new Error('Method not implemented.');
   }
-  getUser(): ConnectingUser {
+  getUser(user: ConnectIngUser): ConnectIngUser {
     return this.user;
   }
 }
