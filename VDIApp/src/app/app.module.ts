36631a7d5c0a11ccdd34615f88a92680e94a6eed
AgentPinky <koeble.soeren@vdi.de>
2020-05-23 13:38:46 +0200

    vorbereitung für die daten von phillip

--- VDIApp/src/app/app.module.ts
+++ VDIApp/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { StaticPageComponent } from './static-page/static-page.component';
-import { AuthService } from './auth/auth.service';
+import { AuthService } from './adapter/linkando/auth/auth.service';
 import { CookieService } from './cookie/cookie.service';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { ChannelOverviewComponent } from './channel-overview/channel-overview.component';
@@ -20,6 +20,10 @@ import {CreatePostComponent} from './create-post/create-post.component';
 import {PostComponent} from './post/post.component';
 import {MatCardModule} from '@angular/material/card';
 import {LoginComponent} from './login/login.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
    declarations: [
@@ -29,7 +33,7 @@ import {LoginComponent} from './login/login.component';
      StaticPageComponent,
      ChannelOverviewComponent,
      CreatePostComponent,
-     PostComponent
+     PostComponent,
 
   ],
   imports: [
@@ -38,7 +42,15 @@ import {LoginComponent} from './login/login.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    MatCardModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [
     AuthService,
