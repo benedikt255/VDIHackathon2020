220c7d492a4d4c7856ab849d31e5691b71a9aa06
benedikt255 <benedikt.geib@googlemail.com>
2020-05-23 21:19:44 +0200

    * redirect für LogIn

--- VDIApp/src/app/login/login.component.ts
+++ VDIApp/src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import {Component, DoCheck, OnInit} from '@angular/core';
 import { ConnectIngCtrl } from '../controller/ConnectIngCtrl';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from './../adapter/linkando/auth/auth.service'; // bypass für den linkando login,
+// für die Seirenvariante brauchen wir da noch was schöneres
+import { environment } from './../../environments/environment'; // umgebungsvariablen, hauptsächlich link auf Pfad
 
 // Beispiel für Integration eines Service mit Interface in Angular
 // https://medium.com/hackernoon/creating-interfaces-for-angular-services-1bb41fbbe47c
@@ -14,6 +17,8 @@ import { Router } from '@angular/router';
   providers: [ConnectIngCtrl]
 })
 export class LoginComponent implements OnInit {
+  // helper für linkandologin
+  private accessCode = '';
 
   private coreCtrl: ConnectIngCtrl;
   private router: Router;
@@ -21,11 +26,21 @@ export class LoginComponent implements OnInit {
   public userName: string;
   public userPassword: string;
 
-  constructor(coreCtrl: ConnectIngCtrl, router: Router) {
+  constructor(coreCtrl: ConnectIngCtrl, router: Router, private route: ActivatedRoute, private authSvc: AuthService) {
     this.router = router;
     this.coreCtrl = coreCtrl;
     this.userName = 'Please insert your Username';
     this.userPassword = 'Please insert your Password';
+    // helper für linkado login, subscribe code param
+    this.route.queryParams.subscribe(params => {
+      this.accessCode = params['code'];
+      if (this.authSvc.getAuth() === '') {
+        if (this.accessCode !== undefined) {
+          this.authSvc.requestAuthWithAccessCode(this.accessCode);
+          this.accessCode = '';
+        }
+      }
+    });
   }
 
   ngOnInit(): void {
@@ -33,13 +48,21 @@ export class LoginComponent implements OnInit {
 
   public Connect(): void
   {
-    this.coreCtrl.connectUser(
+    if (this.authSvc.getAuth() !== '')
+    {
+      this.coreCtrl.connectUser(
       this.userName,
       this.userPassword,
       () => {
+
         this.router.navigateByUrl('welcome');
       }
       );
+    }
+    else
+    {
+      window.location.href = 'https://identity.linkando.co/oauth/auth?redirect_uri=' + encodeURIComponent(environment.authURI);
+    }
   }
 
   public Register(): void{
