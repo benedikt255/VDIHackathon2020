b4cee341952e35b5e69aa77958ac7cd6b7ef8180
moritz.spohr <moritz.spohr@gmx.de>
2020-05-23 12:39:38 +0200

    initial implemention of main-nav component

--- VDIApp/src/app/app.module.ts
+++ VDIApp/src/app/app.module.ts
@@ -1,42 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { MatSliderModule } from '@angular/material/slider';
+import { HttpClientModule } from '@angular/common/http'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { VdiTestComponent } from './vdi-test/vdi-test.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { StaticPageComponent } from './static-page/static-page.component';
-import { AuthService } from './adapter/linkando/auth/auth.service';
+import { AuthService } from './auth/auth.service';
 import { CookieService } from './cookie/cookie.service';
-import { ChannelOverviewComponent } from './channel-overview/channel-overview.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSliderModule} from '@angular/material/slider';
-import {CreatePostComponent} from './create-post/create-post.component';
-import {MatCardModule} from '@angular/material/card';
+import { MainNavComponent } from './main-nav/main-nav.component';
+
 
 @NgModule({
-   declarations: [
-     AppComponent,
-     LoginComponent,
-     StaticPageComponent,
-     ChannelOverviewComponent,
-     CreatePostComponent
+  declarations: [
+    AppComponent,
+    StaticPageComponent,
+    VdiTestComponent,
+    MainNavComponent
   ],
   imports: [
-    MatButtonModule,
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    MatExpansionModule,
-    MatSelectModule,
-    MatCardModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
     AuthService,
