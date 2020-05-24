24843047d785d26c5ebd1fb646972bfc34771d45
Zangarus <marwin.madsen@student.kit.edu>
2020-05-24 14:39:53 +0200

    refactor structure

--- VDIApp/src/app/channel/create-post.component.ts
+++ VDIApp/src/app/channel/create-post.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {ConnectIngBaseService, ConnectIngPost} from '../adapter/base/AbstractBaseService';
+import {ConnectIngBaseService, ConnectIngPost} from '../service/adapter/base/AbstractBaseService';
 import {MatBottomSheetRef} from '@angular/material/bottom-sheet';
 
 @Component({
